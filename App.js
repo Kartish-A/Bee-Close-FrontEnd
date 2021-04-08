5bccf2392fcf74922d59c0f852e3200f44609ab8
Gabriele Pepe <gabriele.pepe@gmail.com>
2021-04-08 14:09:33 +0200

    accepting other changes for merging

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import { Ionicons } from "@expo/vector-icons";
 import { appReducer } from "./appContextReducer";
 
 // importing AuthStack Screens
-<<<<<<< HEAD
 import { LandingPage } from "./screens/authStack/LandingPage";
 import { SignIn } from "./screens/authStack/SignIn";
 import { SignUp } from "./screens/authStack/SignUp";
@@ -17,41 +16,20 @@ import { SignUp } from "./screens/authStack/SignUp";
 import { HiveHome } from "./screens/bottomTabScreens/HiveHome";
 import { CreatePost } from "./screens/bottomTabScreens/CreatePost";
 import { Events } from "./screens/bottomTabScreens/Events";
-import { SellAndBuy } from "./screens/bottomTabScreens/SellAndBuy";
-=======
-import { LandingPage } from './screens/authStack/LandingPage';
-import { SignIn } from './screens/authStack/SignIn';
-import { SignUp } from './screens/authStack/SignUp';
-
-// importing Tab Screens
-import { HiveHome } from './screens/bottomTabScreens/HiveHome';
-import { CreatePost } from './screens/bottomTabScreens/CreatePost';
-import { Events } from './screens/bottomTabScreens/Events'
-import { FreeYourStuff } from './screens/bottomTabScreens/FreeYourStuff'
->>>>>>> main
+import { FreeYourStuff } from "./screens/bottomTabScreens/FreeYourStuff";
 
 //importing DrawerScreens (and custom functions)
 import { CustomDrawerContent } from "./screens/drawerItems/CustomDrawerContent";
 // import { ProfileScreen }from './components/drawerItems/ProfileStack/ProfileScreen';
-<<<<<<< HEAD
 import { SavedItems } from "./screens/drawerItems/SavedItems";
 import { BeesInMyHive } from "./screens/drawerItems/BeesInMyHive";
 import { DirectMessages } from "./screens/drawerItems/DirectMessages";
 
 //importing ProfileStackScreens
-import { ProfileScreen } from "./screens/drawerItems/ProfileStack/ProfileScreen";
-import { EditProfileScreen } from "./screens/drawerItems/ProfileStack/EditProfileScreen";
-=======
-import { SavedItems }from './screens/drawerItems/SavedItems';
-import { BeesInMyHive }from './screens/drawerItems/BeesInMyHive';
-import { DirectMessages }from './screens/drawerItems/DirectMessages';
+import { PersonalProfileScreen } from "./screens/drawerItems/PersonalProfileStack/PersonalProfileScreen";
+import { EditPersonalProfileScreen } from "./screens/drawerItems/PersonalProfileStack/EditPersonalProfileScreen";
 
-//importing ProfileStackScreens
-import { PersonalProfileScreen } from './screens/drawerItems/PersonalProfileStack/PersonalProfileScreen';
-import { EditPersonalProfileScreen } from './screens/drawerItems/PersonalProfileStack/EditPersonalProfileScreen';
-
-import { UserProfileScreen } from './screens/UserProfileScreen'
->>>>>>> main
+import { UserProfileScreen } from "./screens/UserProfileScreen";
 
 const initialState = { isLoggedIn: false };
 //initialising the App context (central storage)
@@ -69,28 +47,98 @@ const PersonalProfileStack = createStackNavigator();
 const HiveHomeStack = createStackNavigator();
 
 //declaring HiveHomeStackScreens
-const HiveHomeStackScreens = () =>(
-  <HiveHomeStack.Navigator initialRouteName='HiveHome' >
-    <HiveHomeStack.Screen name='HiveHome' component={HiveHome} options={{headerShown:false}}/>
-    <HiveHomeStack.Screen name='UserProfile' component={UserProfileScreen} options={{title:'user profile', headerStyle:{backgroundColor:'#37cab8'}, headerTintColor:'#ffffff' }}/>
+const HiveHomeStackScreens = () => (
+  <HiveHomeStack.Navigator initialRouteName="HiveHome">
+    <HiveHomeStack.Screen
+      name="HiveHome"
+      component={HiveHome}
+      options={{ headerShown: false }}
+    />
+    <HiveHomeStack.Screen
+      name="UserProfile"
+      component={UserProfileScreen}
+      options={{
+        title: "user profile",
+        headerStyle: { backgroundColor: "#37cab8" },
+        headerTintColor: "#ffffff",
+      }}
+    />
   </HiveHomeStack.Navigator>
-)
+);
 
 //declaring ProfileStackScreens
-const PersonalProfileStackScreens = () =>(
+const PersonalProfileStackScreens = () => (
   <PersonalProfileStack.Navigator>
-    <PersonalProfileStack.Screen name='PersonalProfileScreen' component={PersonalProfileScreen} options={{headerShown:false}}/>
-    <PersonalProfileStack.Screen name='EditPersonalProfileScreen' component={EditPersonalProfileScreen} options={{title:'Edit profile', headerStyle:{backgroundColor:'#37cab8'},headerTintColor:'#ffffff' }}/>
+    <PersonalProfileStack.Screen
+      name="PersonalProfileScreen"
+      component={PersonalProfileScreen}
+      options={{ headerShown: false }}
+    />
+    <PersonalProfileStack.Screen
+      name="EditPersonalProfileScreen"
+      component={EditPersonalProfileScreen}
+      options={{
+        title: "Edit profile",
+        headerStyle: { backgroundColor: "#37cab8" },
+        headerTintColor: "#ffffff",
+      }}
+    />
   </PersonalProfileStack.Navigator>
-)
+);
 
 //declaring TabsNavigatorScreens
-const TabScreens = ()=>(
-  <Tab.Navigator initialRouteName='HeivHome' tabBarOptions={{activeTintColor:'#37cab8'}}>
-    <Tab.Screen name='HiveHomeStack' component={HiveHomeStackScreens} options={{title:'Home', tabBarIcon:({focused,color,size})=> <Ionicons name="home" size={size} color={color} focused={focused}/>}}/>
-    <Tab.Screen name='CreatePost' component={CreatePost} options={{ tabBarIcon:({focused,color,size})=> <Ionicons name="add-circle-outline" size={size} color={color} focused={focused} />}}/>
-    <Tab.Screen name='Events' component={Events} options={{ tabBarIcon:({focused,color,size})=> <Ionicons name="calendar" size={size} color={color} focused={focused}/>}}/>
-    <Tab.Screen name='FreeYourStuff' component={FreeYourStuff} options={{title:'Free your stuff', tabBarIcon:({focused,color,size})=> <Ionicons name="basket" size={size} color={color} focused={focused}/>}}/>
+const TabScreens = () => (
+  <Tab.Navigator
+    initialRouteName="HeivHome"
+    tabBarOptions={{ activeTintColor: "#37cab8" }}>
+    <Tab.Screen
+      name="HiveHomeStack"
+      component={HiveHomeStackScreens}
+      options={{
+        title: "Home",
+        tabBarIcon: ({ focused, color, size }) => (
+          <Ionicons name="home" size={size} color={color} focused={focused} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="CreatePost"
+      component={CreatePost}
+      options={{
+        tabBarIcon: ({ focused, color, size }) => (
+          <Ionicons
+            name="add-circle-outline"
+            size={size}
+            color={color}
+            focused={focused}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Events"
+      component={Events}
+      options={{
+        tabBarIcon: ({ focused, color, size }) => (
+          <Ionicons
+            name="calendar"
+            size={size}
+            color={color}
+            focused={focused}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="FreeYourStuff"
+      component={FreeYourStuff}
+      options={{
+        title: "Free your stuff",
+        tabBarIcon: ({ focused, color, size }) => (
+          <Ionicons name="basket" size={size} color={color} focused={focused} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
@@ -113,21 +161,26 @@ const AuthStackScreens = () => (
     />
   </AuthStack.Navigator>
 );
-const AppDrawerScreens = ()=>(
-  <AppDrawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}  drawerType='slide' >
-    <AppDrawer.Screen name='Tab' component={TabScreens} />
-    <AppDrawer.Screen name='PersonalPofileScreen' component={PersonalProfileStackScreens} />
-    <AppDrawer.Screen name='BeesInMyHive' component={BeesInMyHive}/>
-    <AppDrawer.Screen name='SavedItems' component={SavedItems}/>
+const AppDrawerScreens = () => (
+  <AppDrawer.Navigator
+    drawerContent={(props) => <CustomDrawerContent {...props} />}
+    drawerType="slide">
+    <AppDrawer.Screen name="Tab" component={TabScreens} />
+    <AppDrawer.Screen
+      name="PersonalPofileScreen"
+      component={PersonalProfileStackScreens}
+    />
+    <AppDrawer.Screen name="BeesInMyHive" component={BeesInMyHive} />
+    <AppDrawer.Screen name="SavedItems" component={SavedItems} />
   </AppDrawer.Navigator>
-)
+);
 
 export default function App() {
   const [state, dispatch] = useReducer(appReducer, initialState);
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <NavigationContainer>
-         {(state.isLoggedIn)?<AppDrawerScreens /> : <AuthStackScreens />}
+        {state.isLoggedIn ? <AppDrawerScreens /> : <AuthStackScreens />}
         {/* <AppDrawerScreens/> */}
       </NavigationContainer>
     </AppContext.Provider>
